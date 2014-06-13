60317ae44dcfa827b1cc7b9099dee4c88431baf5
Mark Madsen <mark@madsenlab.org>
2014-06-13 15:13:58 -0700

    tuning directories for testing

--- install-simupop-for-travis.sh
+++ install-simupop-for-travis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-cd ~/src
+cd /tmp
 wget --no-check-certificate http://iweb.dl.sourceforge.net/project/simupop/simupop/1.1.2/simuPOP-1.1.2.Ubuntu13-x86_64.tar.gz -O simupop-1.1.2.tar.gz
 tar -xzf simupop-1.1.2.tar.gz
 # leaves a directory rooted at usr/
--- runtests-travis.sh
+++ runtests-travis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 cd ~/src
-export PYTHONPATH=$PYTHONPATH:~/src/usr/local/lib/python2.7/dist-packages
+export PYTHONPATH=$PYTHONPATH:/tmp/usr/local/lib/python2.7/dist-packages
 nosetests
